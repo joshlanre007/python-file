6f2a1452a47e1261beb84e298fb97d99bc62860c
joshlanre007 <lanrejoshua3@gmail.com>
2025-07-05 19:43:53 +0100

    assignment 4

---
+++ zero to hero program/assignment 3.py
@@ -0,0 +1,6 @@
+#Write a Python program that computes the greatest common divisor (GCD) of two positive integers
+
+a = int(input("Enter first positive integer: "))
+b = int(input("Enter second positive integer: "))
+GCD = (a,b) 
+print("GCD =", (a,b))
